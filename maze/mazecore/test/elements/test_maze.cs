4895923dc3295b6db4f9f031f6245c461122f432
David Marsh <davemarsh@marshcode.com>
2012-08-21 02:55:35 +0000

    new layout for Tile and Wall storages.

--- maze/mazecore/test/elements/test_maze.cs
+++ maze/mazecore/test/elements/test_maze.cs
@@ -8,6 +8,16 @@ namespace mazecore.elements.test {
     class TestTileStorage {
 
         /************************
+         * Testing Classes
+        ************************/
+        class TestTile : Tile {
+            public TestTile() {
+                //define a parameterless constructor that we can instantiate.  This way we can test the 
+                //storage module without creating any dependicies of the tile.
+            }
+        }
+
+        /************************
          * Static Data and factories
         ************************/
         static int x_range = 10;
@@ -17,7 +27,7 @@ namespace mazecore.elements.test {
             return new TileStorage(TestTileStorage.x_range, TestTileStorage.y_range);
         }
         static Tile create_tile() {
-            return new Tile();
+            return new TestTile();
         }
         /***************************
          * Test Cases
@@ -83,12 +93,6 @@ namespace mazecore.elements.test {
             Tile tile = TestTileStorage.create_tile();
             Assert.Throws<ArgumentOutOfRangeException>(
                     delegate { tile_storage.set_tile(tile, x, y); });
-            
-            
-            
-
-
-
         }
 
         [TestCase(-1, -1)]
@@ -100,8 +104,6 @@ namespace mazecore.elements.test {
         public void test_get_tile_out_of_range(int x, int y) {
             Assert.Throws<ArgumentOutOfRangeException>(
                     delegate { TestTileStorage.create_storage().get_tile(x, y); });
-
-
         }
 
         [TestCase(-1, -1)]
@@ -117,4 +119,90 @@ namespace mazecore.elements.test {
 
 
     }
+
+    [TestFixture]
+    class TestWallStorage {
+
+        /*********************
+         * Test Classes
+         *********************/
+        class TestWall : Wall {
+            public TestWall(){
+                //provide default constructor 
+            }
+        }
+
+        /*******************
+         * Static Methods
+         *******************/
+        static int X_RANGE = 10;
+        static int Y_RANGE = 15;
+
+        static WallStorage create_storage(){
+            return new WallStorage(TestWallStorage.X_RANGE, TestWallStorage.Y_RANGE);
+        }
+        static Wall create_wall() {
+            return new TestWall();
+        }
+        /***********************
+         * Tests
+         **********************/
+
+
+        [Test]
+        public void test_init() {
+            WallStorage wall_storage = TestWallStorage.create_storage();
+            Assert.AreEqual(wall_storage.get_x_range(), 10);
+            Assert.AreEqual(wall_storage.get_y_range(), 15);
+        }
+        [TestCase(0, 0)]
+        [TestCase(-1, 0)]
+        [TestCase(0, -1)]
+        [TestCase(1, 0)]
+        [TestCase(0, 1)]
+        [TestCase(1, -1)]
+        [TestCase(-1, 1)]
+        public void test_bad_init(int x_range, int y_range) {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                    delegate { new WallStorage(x_range, y_range); });
+        }
+        [Test]
+        public void test_get_set_remove_wall_north_south() {
+
+            WallStorage wall_storage = TestWallStorage.create_storage();
+            Wall wall = TestWallStorage.create_wall();
+
+            Assert.Null( wall_storage.get_wall(1, 0, Direction.North) );
+            wall_storage.set_wall(1, 0, Direction.North);
+
+            Assert.AreEqual(wall_storage.get_wall(1, 0, Direction.North), wall);
+            Assert.AreEqual(wall_storage.get_wall(0, 0, Direction.South), wall);
+
+            wall_storage.remove_wall(1, 0, Direction.North);
+            Assert.Null(wall_storage.get_wall(1, 0, Direction.North));
+            Assert.Null(wall_storage.get_wall(0, 0, Direction.South));
+        }
+
+        [Test]
+        public void test_get_set_remove_wall_east_west() {
+
+            WallStorage wall_storage = TestWallStorage.create_storage();
+            Wall wall = TestWallStorage.create_wall();
+
+            Assert.Null(wall_storage.get_wall(1, 0, Direction.East));
+            wall_storage.set_wall(1, 0, Direction.East);
+
+            Assert.AreEqual(wall_storage.get_wall(1, 0, Direction.East), wall);
+            Assert.AreEqual(wall_storage.get_wall(1, 1, Direction.West), wall);
+
+            wall_storage.remove_wall(1, 0, Direction.East);
+            Assert.Null(wall_storage.get_wall(1, 0, Direction.East));
+            Assert.Null(wall_storage.get_wall(1, 1, Direction.West));
+        }
+
+
+
+    }
+
+
 }
