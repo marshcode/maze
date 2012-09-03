05244d2cee45eea32154118b06041af39b9ef0a2
David Marsh <davemarsh@marshcode.com>
2012-09-03 05:04:17 +0000

    console maze glyphs

--- consolemaze/art/glyph.cs
+++ consolemaze/art/glyph.cs
@@ -1,1 +1,37 @@
-﻿
+﻿namespace consolemaze.art {
+
+    using System.Collections.Generic;
+
+    class Glyph<T>{
+
+        protected Dictionary<T, char> mapping;
+        protected char default_value;
+
+        public Glyph(char default_value) {
+            this.mapping = new Dictionary<T, char>();
+            this.default_value = default_value;
+        }
+
+        public void add_character(T key, char c) {
+            this.remove_character(key);
+            this.mapping.Add(key, c);
+        }
+        public char get_character(T key) { 
+            try{
+                return this.mapping[key];
+            }catch(KeyNotFoundException){
+                return this.get_default();
+            }
+        }
+        public void remove_character(T key) {
+            try {
+                this.mapping.Remove(key);
+            }catch(KeyNotFoundException){}
+        }
+        
+        public char get_default() {
+            return this.default_value;
+        }
+        
+    }
+}
--- consolemaze/test/art/test_glyph.cs
+++ consolemaze/test/art/test_glyph.cs
@@ -1,9 +1,72 @@
-﻿namespace consolemaze.glyph.test {
+﻿namespace consolemaze.art.test {
 
     using NUnit.Framework;
+    using consolemaze.art;
 
     [TestFixture]
     class TestGlyph {
+
+        enum Blah { one, two, three };
+
+        [TestCase]
+        public void test_with_enum() {
+            //silly, but an important part of the class.\
+            Glyph<Blah> g = new Glyph<Blah>(' ');
+
+            g.add_character(Blah.one, 'a');
+            Assert.AreEqual(g.get_character(Blah.one), 'a');
+            
+
+        }
+
+        [TestCase]
+        public void test_get_default() {
+            Glyph<int> g = new Glyph<int>(' ');
+            Assert.AreEqual(g.get_default(), ' ');
+        }
+
+        [TestCase]
+        public void test_remove_non_existent_entry() {
+            Glyph<int> g = new Glyph<int>(' ');
+            Assert.AreEqual(g.get_character(1), ' ');
+            g.remove_character(1);
+            Assert.AreEqual(g.get_character(1), ' ');
+        }
+
+
+        [TestCase]
+        public void test_overwrite() {
+            Glyph<int> g = new Glyph<int>(' ');
+
+            g.add_character(1, 'a');
+            Assert.AreEqual(g.get_character(1), 'a');
+
+            g.add_character(1, 'b');
+            Assert.AreEqual(g.get_character(1), 'b');
+
+        }
+
+        [TestCase]
+        public void test_add_get_remove_glyph() {
+
+            Glyph<int> g = new Glyph<int>(' ');
+            Assert.AreEqual(g.get_character(1), ' ');
+            Assert.AreEqual(g.get_character(2), ' ');
+
+            g.add_character(1, 'q');
+            g.add_character(2, 'a');
+
+            Assert.AreEqual(g.get_character(1), 'q');
+            Assert.AreEqual(g.get_character(2), 'a');
+
+            g.remove_character(1);
+            g.remove_character(2);
+
+            Assert.AreEqual(g.get_character(1), ' ');
+            Assert.AreEqual(g.get_character(2), ' ');
+
+        }
+
 
     }
 
