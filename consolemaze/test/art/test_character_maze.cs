1885fa2cd1f545b8e5a5752c6c12f4abe530d8b6
David Marsh <davemarsh@marshcode.com>
2012-09-04 02:39:53 +0000

    added complex test case.

--- consolemaze/test/art/test_character_maze.cs
+++ consolemaze/test/art/test_character_maze.cs
@@ -7,6 +7,8 @@
     using mazecore.elements;
     using mazecore.direction;
 
+    using mazecore.elements.test;
+
     [TestFixture]
     class TestCharacterMaze {
 
@@ -27,6 +29,57 @@
             foreach (Direction dir in directions) {
                 Wall wall = new Wall(tile.get_maze(), x, y, dir); 
             }
+
+        }
+
+
+        [Test]
+        public void test_complex() {
+            Maze maze = new Maze(10, 10);
+
+            Tile t = new Tile(maze, 0, 2);
+            Wall w = new Wall(maze, 0, 2, Direction.North);
+            w = new Wall(maze, 0, 2, Direction.West);
+            w = new Wall(maze, 0, 2, Direction.East);
+            Character character = new Character(maze, 0, 2);
+
+            t = new Tile(maze, 0, 1);
+            w = new Wall(maze, 0, 1, Direction.West);
+
+            t = new Tile(maze, 1, 1);
+            w = new Wall(maze, 1, 1, Direction.North);
+            w = new Wall(maze, 1, 1, Direction.East);
+            w = new Wall(maze, 1, 1, Direction.South);
+
+            t = new Tile(maze, 0, 0);
+            w = new Wall(maze, 0, 0, Direction.West);
+            w = new Wall(maze, 0, 0, Direction.South);
+
+            t = new Tile(maze, 1, 0);
+            w = new Wall(maze, 1, 0, Direction.South);
+
+            t = new Tile(maze, 2, 0);
+            w = new Wall(maze, 2, 0, Direction.South);
+            w = new Wall(maze, 2, 0, Direction.North);
+
+
+            t = new Tile(maze, 3, 0);
+            w = new Wall(maze, 3, 0, Direction.South);
+            w = new Wall(maze, 3, 0, Direction.North);
+
+            t = new Tile(maze, 4, 0);
+            w = new Wall(maze, 4, 0, Direction.South);
+            w = new Wall(maze, 4, 0, Direction.North);
+
+
+            t = new Tile(maze, 5, 0);
+            w = new Wall(maze, 5, 0, Direction.South);
+            w = new Wall(maze, 5, 0, Direction.East);
+            w = new Wall(maze, 5, 0, Direction.North);
+
+
+            CharacterMaze character_maze = new CharacterMaze(maze);
+            Assert.True( this.render_compare(character_maze, "blah"));
 
         }
 
